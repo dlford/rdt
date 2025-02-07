e62a77cc1c220331c02026153ee485ee8997d353
dlford <22881850+dlford@users.noreply.github.com>
2025-02-07 16:17:50 -0700

    GROK-2507: setting up db connection

--- node/lib/server.js
+++ node/lib/server.js
@@ -1,11 +1,32 @@
+const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 
-const app = express();
+const url = 'mongodb://mongo:27017';
+const dbName = 'rdt';
+const client = new MongoClient(url);
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
+client
+  .connect((err) => {
+    if (err) throw err;
+  })
+  .then((client) => {
+    const db = client.db(dbName);
 
-app.listen(80, () => {
-	console.log('Example app listening on port 80!');
-});
+    const app = express();
+
+    app.get('/', (req, res) => {
+      // db.collection('movies')
+      //   .find()
+      //   .toArray((err, result) => {
+      //     if (err) throw err;
+
+      //     console.log(result);
+      //   });
+
+      res.send('Hello World!');
+    });
+
+    app.listen(80, () => {
+      console.log('Example app listening on port 80!');
+    });
+  });
