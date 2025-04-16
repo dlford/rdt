7adbf8810abdef9755364867c20a9040b2d86058
dlford <22881850+dlford@users.noreply.github.com>
2025-04-16 16:38:25 -0700

    GROK-2511: Add DB connection to GraphQL context

--- graphql/lib/server.js
+++ graphql/lib/server.js
@@ -1,16 +1,32 @@
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import { MongoClient, ObjectId } from 'mongodb';
 import cors from 'cors';
 import express from 'express';
 
 const startTime = Date.now();
 
 const app = express();
+
 const port = process.env.RDT_GRAPHQL_PORT || 4000;
+const dbUrl = process.env.RDT_GRAPHQL_DB_URL || 'mongodb://mongo:27017';
+const dbName = process.env.RDT_GRAPHQL_DB_NAME || 'rdt';
+
+const dbClient = new MongoClient(dbUrl);
+
+async function runMigrations(db) {
+  await db.createCollection('movies');
+}
 
 const typeDefs = `#graphql
   type Status {
     start: Float
+  }
+
+  type Training_Movies_Movie {
+    id: String
+    title: String
+    release_date: String
   }
 
   type Book {
@@ -40,7 +56,12 @@ const resolvers = {
     status: () => ({
       start: startTime,
     }),
-    books: () => books,
+    books: async (parent, args, ctx) => {
+      // TODO: Delete test
+      const movies = await ctx.db.collection('movies').find().toArray();
+      console.log(movies);
+      return books;
+    },
   },
 };
 
@@ -56,7 +77,28 @@ app.get('/status', (req, res) => {
   });
 });
 
-app.use('/', cors(), express.json(), expressMiddleware(server));
+app.use(
+  '/',
+  cors(),
+  express.json(),
+  expressMiddleware(server, {
+    context: async () => {
+      await dbClient.connect((err) => {
+        if (err) throw err;
+      });
+
+      process.on('exit', async () => {
+        await dbClient.close();
+      });
+
+      const db = dbClient.db(dbName);
+
+      await runMigrations(db);
+
+      return { db };
+    },
+  }),
+);
 
 app.listen(port, () => {
   console.log(`GraphQL listening on port ${port}!`);
